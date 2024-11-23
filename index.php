60fbfb94ae6b9ccdc124b609313af7140548b889
rankadian <dianthafafian@gmail.com>
2024-11-23 18:54:16 +0700

    delete menu feature

--- index.php
+++ index.php
@@ -51,7 +51,6 @@
         <ul>
           <li><a href="#hero" class="active">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#features">Features</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
