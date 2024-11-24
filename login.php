958238265276bae6b437df91613ba971475c9612
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-11-24 15:45:32 +0700

    proses lofin eror pass

--- login.php
+++ login.php
@@ -23,9 +23,13 @@ if (isset($_SESSION['role'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/main.css">
 </head>
+<!--body begin -->
 <body>
     <div class="d-flex justify-content-center align-items-center vh-100 bg-light">
         <div class="card shadow p-4" style="width: 100%; max-width: 400px;">
+            <div style="text-align: center; margin-bottom: 2px;">
+                <img src="./assets/img/sibetaV3.png" alt="icon" style="max-width: 150px; width: 100%; height: auto;">
+            </div>
             <h2 class="text-center mb-4">Login - <strong>SIBETA</strong></h2>
             <form action="process_login.php" method="POST">
                 <div class="mb-3">
@@ -36,13 +40,17 @@ if (isset($_SESSION['role'])) {
                     <label for="password" class="form-label">Password</label>
                     <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password" required>
                 </div>
-                <button type="submit" class="btn btn-primary w-100">Login</button>
+                
+                <button type="submit" class="btn btn-primary w-100" style="background-color:#1b156a; border-color:#1b156a; color:white;">Login</button>
             </form>
             <p class="text-center mt-3">Belum punya akun? <a href="register.php" class="text-decoration-none">Daftar</a></p>
         </div>
     </div>
 
+
+
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+<!--body end -->
 </html>
