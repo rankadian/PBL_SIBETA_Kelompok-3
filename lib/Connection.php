042bf3355dfdab6f360866cc4439e31babd2e43d
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-12-03 18:42:50 +0700

    connection my laptop sqlsrv

--- lib/Connection.php
+++ lib/Connection.php
@@ -1,6 +1,6 @@
 <?php
 $use_driver = 'sqlsrv'; // mysql atau sqlsrv
-$host = 'LAPTOP-9E916KGJ'; // 'localhost';
+$host = 'LAPTOP-Q3PDL8SK'; // 'localhost';
 $username = ''; //'sa';
 $password = '';
 $database = 'sibeta'; // 'master'
