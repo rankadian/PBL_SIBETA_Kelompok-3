0e129325777315b02c22107f57351abb5eadcd68
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-12-28 00:07:51 +0700

    modify download lebih tertata

--- source/pages/downloadMhs.php
+++ source/pages/downloadMhs.php
@@ -1,41 +1,30 @@
 <div class="card">
     <div class="card-header">
-        <h3 class="card-title">Layanan Bebas Tanggungan</h3>
+        <h3 class="card-title font-weight-bold" style="font-family: 'Arial', sans-serif;">Layanan Bebas Tanggungan</h3>
     </div>
     <div class="card-body">
-        <h4>DOWNLOAD SURAT</h4>
-        <ul>
-            <li>DOWNLOAD Surat Publikasi *admin prodi
+        <h4 class="font-weight-bold" style="font-family: 'Arial', sans-serif;">DOWNLOAD SURAT</h4>
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Publikasi </h5>
                 <!-- Button to Download Surat Publikasi -->
-                <a href="https://drive.google.com/file/d/1nucVO1ewGTILmydCNr6hOddcePdRb7yR/view?usp=sharing" class="btn btn-sm btn-primary">Download Surat Publikasi</a>
-            </li>
-            <li>DOWNLOAD Bebas Kompen *admin prodi
+                <a href="https://drive.google.com/file/d/1nucVO1ewGTILmydCNr6hOddcePdRb7yR/view?usp=sharing" class="btn btn-sm btn-primary" target="_blank">Download Surat Publikasi</a>
+            </div>
+            <div class="col-md-6 mb-3">
+                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Bebas Kompen </h5>
                 <!-- Button to Download Surat Bebas Kompen -->
-                <a href="https://drive.google.com/file/d/1U5hUtjeEpa9DADH4FzXbja7ty5Lb6uf_/view?usp=drive_link" class="btn btn-sm btn-primary">Download Surat Bebas Kompen</a>
-            </li>
-            <li>DOWNLOAD Validasi PKL *admin ruang BACA
+                <a href="https://drive.google.com/file/d/1U5hUtjeEpa9DADH4FzXbja7ty5Lb6uf_/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat Bebas Kompen</a>
+            </div>
+            <div class="col-md-6 mb-3">
+                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Validasi PKL </h5>
                 <!-- Button to Download Surat Validasi PKL -->
-                <a href="https://drive.google.com/file/d/17XeqjkVv6YGyfiP97ET-XUU9oE-p3y4t/view?usp=drive_link" class="btn btn-sm btn-primary">Download Surat Validasi PKL</a>
-            </li>
-            <li>DOWNLOAD Surat SKLA *admin bu merry
+                <a href="https://drive.google.com/file/d/17XeqjkVv6YGyfiP97ET-XUU9oE-p3y4t/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat Validasi PKL</a>
+            </div>
+            <div class="col-md-6 mb-3">
+                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat SKLA </h5>
                 <!-- Button to Download Surat SKLA -->
-                <a href="https://drive.google.com/file/d/1thBugXeZPZlxWzp169e-Zf9Qmy3SwdRS/view?usp=drive_link" class="btn btn-sm btn-primary">Download Surat SKLA</a>
-            </li>
-        </ul>
-        <!-- Button to Download All Files -->
-        <!-- <button class="btn btn-sm btn-success" onclick="downloadAllFiles()">Download Semua Surat</button> -->
+                <a href="https://drive.google.com/file/d/1thBugXeZPZlxWzp169e-Zf9Qmy3SwdRS/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat SKLA</a>
+            </div>
+        </div>
     </div>
 </div>
-
-
-<!-- <script>
-    // Function to handle Download of Single File (e.g., 'Surat Bebas Tanggungan')
-    function downloadSingleFile(fileUrl) {
-        window.location.href = fileUrl;  // Redirect to the file URL in the 'download' folder
-    }
-
-    // Function for Downloading All Files (ZIP)
-    function downloadAllFiles() {
-        window.location.href = 'downloadAllFiles.php';  // Redirect to PHP script to download ZIP
-    }
-</script> -->
