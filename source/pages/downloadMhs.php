50b8fe46d3e08fc989c354667963a8e3db5c18e1
rankadian <dianthafafian@gmail.com>
2024-12-30 07:36:55 +0700

    delete double download

--- source/pages/downloadMhs.php
+++ source/pages/downloadMhs.php
@@ -4,7 +4,7 @@
         <!-- Dashboard Header -->
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Beranda</h1>
+                <h1 style="font-family: 'Arial', sans-serif;">Download Surat</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
@@ -17,35 +17,34 @@
 
     <!-- Layanan Bebas Tanggungan Section -->
     <div class="card">
-    <div class="card-header">
-        <h3 class="card-title font-weight-bold" style="font-family: 'Arial', sans-serif;">Layanan Bebas Tanggungan</h3>
-    </div>
-    <div class="card-body">
-        <h4 class="font-weight-bold" style="font-family: 'Arial', sans-serif;">DOWNLOAD SURAT</h4>
-        <div class="row">
-            <div class="col-md-6 mb-3">
-                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Publikasi </h5>
-                <!-- Button to Download Surat Publikasi -->
-                <a href="https://drive.google.com/file/d/1nucVO1ewGTILmydCNr6hOddcePdRb7yR/view?usp=sharing" class="btn btn-sm btn-primary" target="_blank">Download Surat Publikasi</a>
-            </div>
-            <div class="col-md-6 mb-3">
-                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Bebas Kompen </h5>
-                <!-- Button to Download Surat Bebas Kompen -->
-                <a href="https://drive.google.com/file/d/1U5hUtjeEpa9DADH4FzXbja7ty5Lb6uf_/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat Bebas Kompen</a>
-            </div>
-            <div class="col-md-6 mb-3">
-                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Validasi PKL </h5>
-                <!-- Button to Download Surat Validasi PKL -->
-                <a href="https://drive.google.com/file/d/17XeqjkVv6YGyfiP97ET-XUU9oE-p3y4t/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat Validasi PKL</a>
-            </div>
-            <div class="col-md-6 mb-3">
-                <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat SKLA </h5>
-                <!-- Button to Download Surat SKLA -->
-                <a href="https://drive.google.com/file/d/1thBugXeZPZlxWzp169e-Zf9Qmy3SwdRS/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat SKLA</a>
+        <div class="card-header">
+            <h3 class="card-title font-weight-bold" style="font-family: 'Arial', sans-serif;">Layanan Bebas Tanggungan</h3>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Publikasi </h5>
+                    <!-- Button to Download Surat Publikasi -->
+                    <a href="https://drive.google.com/file/d/1nucVO1ewGTILmydCNr6hOddcePdRb7yR/view?usp=sharing" class="btn btn-sm btn-primary" target="_blank">Download Surat Publikasi</a>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Bebas Kompen </h5>
+                    <!-- Button to Download Surat Bebas Kompen -->
+                    <a href="https://drive.google.com/file/d/1U5hUtjeEpa9DADH4FzXbja7ty5Lb6uf_/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat Bebas Kompen</a>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat Validasi PKL </h5>
+                    <!-- Button to Download Surat Validasi PKL -->
+                    <a href="https://drive.google.com/file/d/17XeqjkVv6YGyfiP97ET-XUU9oE-p3y4t/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat Validasi PKL</a>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <h5 class="font-weight-semibold" style="font-family: 'Arial', sans-serif;">Surat SKLA </h5>
+                    <!-- Button to Download Surat SKLA -->
+                    <a href="https://drive.google.com/file/d/1thBugXeZPZlxWzp169e-Zf9Qmy3SwdRS/view?usp=drive_link" class="btn btn-sm btn-primary" target="_blank">Download Surat SKLA</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
     </div>
